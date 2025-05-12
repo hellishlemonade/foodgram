6a9867ea94c8a13db303aceafcd01b583aab1712
Dmitriy Lyapin <hellishlemonade111@gmail.com>
2025-05-12 20:27:45 +0300

    Create custom user in users app

---
+++ backend/users/models.py
@@ -0,0 +1,47 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.validators import UnicodeUsernameValidator
+
+
+MAX_LENGTH_USERNAME = 150
+MAX_LENGTH_NAME = 150
+
+
+class Profile(AbstractUser):
+    """
+    Кастомная модель юзера, переопределяющая необязательные
+    поля в обязательные.
+
+    Добавлено поле для фотографии профиля.
+    """
+    username = models.CharField(
+        'Никнейм',
+        max_length=MAX_LENGTH_USERNAME,
+        unique=True,
+        validators=[UnicodeUsernameValidator()]
+    )
+
+    email = models.EmailField(
+        'Email',
+        unique=True,
+        blank=False
+    )
+
+    first_name = models.CharField(
+        'Имя',
+        max_length=MAX_LENGTH_NAME,
+        blank=False,
+        null=False
+    )
+
+    last_name = models.CharField(
+        'Фамилия',
+        max_length=MAX_LENGTH_NAME,
+        blank=False,
+        null=False
+    )
+
+    avatar = models.ImageField(
+        upload_to='users/',
+        blank=True
+    )
