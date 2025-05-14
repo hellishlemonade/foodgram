c842474e3d6a88b2391a3584d3bf24ca0795f781
Dmitriy Lyapin <hellishlemonade111@gmail.com>
2025-05-14 20:37:57 +0300

    Create serializers for image and change password

--- backend/api/v1/serializers.py
+++ backend/api/v1/serializers.py
@@ -1,13 +1,55 @@
+import base64
+import uuid
+
 from djoser.serializers import UserCreateSerializer
+from django.core.files.base import ContentFile
 from django.contrib.auth import get_user_model
+from django.contrib.auth.password_validation import validate_password
+from rest_framework import serializers
 
 
 User = get_user_model()
 
 
+class Base64ImageField(serializers.ImageField):
+    def to_internal_value(self, data):
+        if isinstance(data, str) and data.startswith('data:image'):
+            format, imgstr = data.split(';base64,')
+            ext = format.split('/')[-1]
+            data = ContentFile(
+                base64.b64decode(imgstr), name=f'{uuid.uuid4()}.' + ext)
+            print(data)
+        return super().to_internal_value(data)
+
+
+class ImageSerializer(serializers.ModelSerializer):
+    avatar = Base64ImageField(required=False, allow_null=True)
+
+    class Meta:
+        model = User
+        fields = ('avatar',)
+
+
 class MyUserCreateSerializer(UserCreateSerializer):
+    avatar = Base64ImageField(required=False, allow_null=True)
 
     class Meta(UserCreateSerializer.Meta):
         model = User
         fields = (
-            'email', 'id', 'username', 'first_name', 'last_name', 'password')
+            'email',
+            'id',
+            'username',
+            'first_name',
+            'last_name',
+            'password',
+            'avatar',
+        )
+
+
+class PasswordChangeSerializer(serializers.Serializer):
+    current_password = serializers.CharField(required=True)
+    new_password = serializers.CharField(required=True)
+
+    def validate_new_password(self, value):
+        validate_password(value)
+        return value
